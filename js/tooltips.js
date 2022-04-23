9a70611767084d304fe513a66d77f257315d02e5
Kaan Doğanay <83249294+toasting-dev@users.noreply.github.com>
2022-04-23 22:38:08 +0300

    Add files via upload

---
+++ js/tooltips.js
@@ -0,0 +1,43 @@
+tippy("#statusIcon", {
+  allowHTML: true,
+  content: "Loading...",
+  placement: "right-end",
+  arrow: false,
+});
+
+tippy("#themeButton", {
+  content: "Switch theme",
+  placement: "left",
+});
+
+tippy("#email", {
+  content: "Click to email me",
+  placement: "right",
+  arrow: false,
+});
+
+tippy("#discord", {
+  content: "Toasting#4702",
+  placement: "bottom",
+});
+
+tippy("#github", {
+  content: "toasting-dev",
+  placement: "bottom",
+});
+
+tippy("#instagram", {
+  content: "@mustafa.demirtra",
+  placement: "bottom",
+});
+
+
+tippy("#english", {
+  content: "English (B1)",
+  placement: "bottom",
+});
+
+tippy("#turkish", {
+  content: "Turkish (native)",
+  placement: "bottom",
+});
